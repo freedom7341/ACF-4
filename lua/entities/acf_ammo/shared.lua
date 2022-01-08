b241ef74c1549f80aa165b4125de06a8d0b4408b
freedom <36094486+freedom7341@users.noreply.github.com>
2022-01-08 13:27:55 -0700

    debloated acf4
    no more artillery no more guns no more bombs

--- lua/entities/acf_ammo/shared.lua
+++
@@ -1,8 +0,0 @@
-DEFINE_BASECLASS("acf_base_scalable")
-
-ENT.PrintName     = "ACF Ammo Crate"
-ENT.WireDebugName = "ACF Ammo Crate"
-ENT.PluralName    = "ACF Ammo Crates"
-ENT.IsAmmoCrate   = true
-
-cleanup.Register("acf_ammo")
--- lua/entities/acf_gun/shared.lua
+++
@@ -1,9 +0,0 @@
-DEFINE_BASECLASS("acf_base_scalable")
-
-ENT.PrintName     = "ACF Weapon"
-ENT.WireDebugName = "ACF Weapon"
-ENT.PluralName    = "ACF Weapons"
-ENT.IsWeapon      = true
-
-cleanup.Register("acf_gun")
-cleanup.Register("acf_smokelauncher")
